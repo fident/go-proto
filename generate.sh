7ceb286301347de966345f0d7de47062da72b425
Luke Hines <lukehines@protonmail.com>
2017-05-10 11:28:54 +0100

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-FILES=vendor/github.com/fident/proto/*.proto
+FILES=vendor/github.com/LucidCube/proto-fident/*.proto
 for f in $FILES
 do
   file=$(basename "$f")
   package="${file%.*}"
   echo "Processing $package $f"
   mkdir -p $package
-  protoc -I vendor/github.com/fident/proto --go_out=plugins=grpc:$package $f
+  protoc -I vendor/github.com/LucidCube/proto-fident --go_out=plugins=grpc:$package $f
   git add $package/*
 done
